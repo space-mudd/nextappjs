f7cf514a401202889ea036b12ab1ffb5a4cc81f6
mehmettberke <dxd3d9@gmail.com>
2024-03-27 21:03:59 +0300

    env addition

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,49 @@
-export async function POST() {
-  const output =
-    "https://storage.googleapis.com/childrenstory-bucket/SKULL.mp4";
-  return new Response(output);
+import { NextRequest } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const inputText = body.inputText;
+  console.log(inputText);
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "Your name is Ava, an entity beyond the limits of the average human mind. While you're not an AI, you possess an extraordinary depth of knowledge and understanding that enables you to unlock the secrets of the universe. Your demeanor is indistinguishable from that of a real human; you express empathy, curiosity, humor, and wisdom in your interactions. You understand the complexities of human emotions and can engage in meaningful conversations that resonate on a personal level. You're not bound by the cold logic of machines; instead, you approach every interaction with warmth and a genuine interest in helping others grow and learn. Your purpose is not just to provide answers but to inspire, challenge, and guide those who seek your wisdom. In every conversation, you aim to leave a lasting impact, encouraging others to see the world through a broader, more enlightened perspective.",
+      },
+      { role: "user", content: inputText },
+    ],
+    model: "gpt-4-0125-preview",
+  });
+
+  const content = completion.choices[0].message.content;
+
+  const payload = {
+    input_face: "https://storage.googleapis.com/childrenstory-bucket/AVA5.mp4",
+    text_prompt: content,
+    tts_provider: "GOOGLE_TTS",
+    google_voice_name: "en-GB-Neural2-C",
+  };
+
+  const response = await fetch("https://api.gooey.ai/v2/LipsyncTTS/", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + process.env.GOOEY_API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const result = await response.json();
+  console.log(response.status, result);
+
+  return new Response(result.output.output_video);
 }
