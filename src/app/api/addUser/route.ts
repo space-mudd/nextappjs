e1e93fc3cd137c1b882da3eee9ad40d78b891731
mehmettberke <dxd3d9@gmail.com>
2024-03-29 21:27:25 +0300

    add user

--- src/app/api/addUser/route.ts
+++ src/app/api/addUser/route.ts
@@ -1,3 +1,8 @@
+export async function POST() {
+  return new Response("OK");
+}
+
+/*
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
@@ -17,12 +22,12 @@ const createOrUpdateUser = async (userId: string) => {
       userId: userId,
     },
     UpdateExpression:
-      "SET conversationCount = if_not_exists(conversationCount, :initialCount)",
+    "SET conversationCount = if_not_exists(conversationCount, :initialCount)",
     ExpressionAttributeValues: {
       ":initialCount": 3,
     },
   });
-
+  
   try {
     const response = await docClient.send(command);
     console.log(response);
@@ -35,17 +40,17 @@ const createOrUpdateUser = async (userId: string) => {
 export async function POST(request: Request) {
   const cookies = request.headers.get("cookie");
   let userId = null;
-
+  
   if (cookies) {
     const parsedCookies = cookies.split("; ").reduce((acc, cookie) => {
       const [name, value] = cookie.split("=");
       acc[name] = value;
       return acc;
     }, {} as Record<string, string>);
-
+    
     userId = parsedCookies["userId"];
   }
-
+  
   if (!userId) {
     userId = uuidv4(); // Benzersiz bir kullanıcı kimliği oluştur
     await createOrUpdateUser(userId);
@@ -53,10 +58,10 @@ export async function POST(request: Request) {
   } else {
     console.log("Existing user retrieved");
   }
-
+  
   const sessionToken = uuidv4();
   // Oturum belirtecini ve kullanıcı kimliğini veritabanına veya önbelleğe kaydet
-
+  
   const response = new Response(JSON.stringify({ sessionToken }), {
     status: 200,
     headers: {
@@ -64,6 +69,8 @@ export async function POST(request: Request) {
       "Set-Cookie": `userId=${userId}; HttpOnly; Secure; SameSite=Strict`,
     },
   });
-
+  
   return response;
 }
+
+*/
