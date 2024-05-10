45fd187d2e2ff86d51df5c244c2d5511fdf33d73
mehmettberke <dxd3d9@gmail.com>
2024-05-10 12:51:25 +0300

    link has been sent

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -161,7 +161,9 @@ function SignInForm({ showForm, setShowForm }: SignInFormProps) {
                   Are You HUMAN? Please Verify.
                 </DialogTitle>
                 <DialogDescription>
-                  <div className="text-xl">Link has been sent</div>
+                  <div className="text-xl">
+                    Link has been sent. Please check your e-mail.
+                  </div>
                   <div
                     onClick={() => setShowForm(false)}
                     className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
